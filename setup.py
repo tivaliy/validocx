6db0f48963996b31ad9fcf11e8c08d94038e1ec0
Vitalii Kulanov <vitaliy@kulanov.org.ua>
2017-09-11 12:11:29 +0300

    Add Travis CI support

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ classifiers = [
     'Operating System :: POSIX :: Linux',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Topic :: Text Processing :: General',
